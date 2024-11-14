a7c7079ed90b6cdcdc0338896a68ca8d6a4c62ee
michelladambento <michelladamb@gmail.com>
2024-11-13 22:19:49 -0300

    estudo sobre objetos

---
+++ objetos/Fruta.dart
@@ -0,0 +1,13 @@
+class Fruta{
+
+  String nome;
+
+  String cor;
+
+  Fruta( this.nome, this.cor);
+
+  String toString(){
+    return "Nome fruta: ${this.nome}\nCor da fruta: ${this.cor}\n";
+  }
+
+}
---
+++ objetos/main.dart
@@ -0,0 +1,11 @@
+import 'Fruta.dart';
+
+void main(){
+
+  Fruta uva = new Fruta("Uva", "roxo");
+  print(uva.toString());
+
+  Fruta abacaxi = new Fruta("Abacaxi", "amarelo");
+  print(abacaxi.toString());
+
+}
