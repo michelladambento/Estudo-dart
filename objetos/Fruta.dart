108d1736774c9efe64fc9cd0f7335a3a4d21409a
michelladambento <michelladamb@gmail.com>
2024-11-13 22:34:56 -0300

    estudo sobre objetos

--- objetos/Fruta.dart
+++ objetos/Fruta.dart
@@ -6,6 +6,15 @@ class Fruta{
 
   Fruta( this.nome, this.cor);
 
+  // contrutor nomeado
+  Fruta.nomeados({required this.nome, required this.cor});
+
+  // construtor com operações
+  Fruta.maiusculas(this.nome, this.cor){
+    nome = nome.toUpperCase();
+    cor = cor.toUpperCase();
+  }
+
   String toString(){
     return "Nome fruta: ${this.nome}\nCor da fruta: ${this.cor}\n";
   }
--- objetos/main.dart
+++ objetos/main.dart
@@ -2,11 +2,19 @@ import 'Fruta.dart';
 
 void main(){
 
-// new é opcional
+  // new é opcional
   Fruta uva = Fruta("Uva grape", "roxo");
   print(uva.toString());
 
   Fruta abacaxi = new Fruta("Abacaxi", "amarelo");
   print(abacaxi.toString());
+
+  // chamando construtor nomeado
+  Fruta limao = Fruta.nomeados(nome: "Limão", cor: "verde");
+  print(limao.toString());
+
+  // chamando construtor nomeado com execução de operações
+  Fruta abacate = Fruta.maiusculas("Abacate", "verde");
+  print(abacate.toString());
 
 }
