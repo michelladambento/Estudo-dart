9a0cfbe26c4898b0da43f06385824b03cf86bd41
michelladambento <michelladamb@gmail.com>
2024-11-16 17:44:20 -0300

    estudo sobre maps

---
+++ collections/pessoas.dart
@@ -0,0 +1,26 @@
+void main(){
+
+  Pessoa pessoa = Pessoa("Michell Adam Bento",32, 1.70);
+  print(pessoa.toMap());
+
+}
+
+class Pessoa{
+
+  String nome;
+
+  int idade;
+
+  double altura;
+
+  Pessoa(this.nome, this.idade, this.altura);
+
+  Map<String, dynamic> toMap(){
+    Map<String, dynamic> resultado = {};
+    resultado["nome"] = this.nome;
+    resultado["idade"] = this.idade;
+    resultado["altura"] = this.altura;
+    return resultado;
+  }
+
+}
