6ec3fd2aff1c3cd0b4249cc70de1b8c0003c98f1
michelladambento <michelladamb@gmail.com>
2024-11-16 16:30:47 -0300

    estudo de enums

---
+++ collections/main.dart
@@ -0,0 +1,23 @@
+import 'dart:ffi';
+
+void main(){
+
+  // List deixa incluir dados repetidos
+  List<String> destinos = [];
+  destinos.add("Rio de Janeiro");
+  destinos.add("Rio de Janeiro");
+  destinos.add("São Paulo");
+  print(destinos);
+
+  // Set não deixa incluir dados repetidos
+  Set<String> destinosVisitados = <String>{};
+  destinosVisitados.add("Rio de Janeiro");
+  destinosVisitados.add("Rio de Janeiro");
+  destinosVisitados.add("São Paulo");
+  print(destinosVisitados);
+
+  //saída final
+  //[Rio de Janeiro, Rio de Janeiro, São Paulo]  LIST
+  //{Rio de Janeiro, São Paulo}  SET
+
+}
